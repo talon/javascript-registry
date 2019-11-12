3adae4fb40b6845827fa07a918691c02689ea1bb
Talon <hire@talon.computer>
2019-11-12 16:26:53 -0500

    chore(error handling): npm run new handles errors a little better now

--- scripts/new/index.js
+++ scripts/new/index.js
@@ -1,48 +1,64 @@
 import fs from "fs";
+import assert from "assert";
 import prompts from "prompts";
 import child_process from "child_process";
 import { promisify } from "util";
 import { resolve } from "path";
 import files from "./files";
 
-(async (mkdir, writeFile, spawn) => {
-  const DEV_DEPENDENCIES = [
-    "@babel/core",
-    "@babel/preset-env",
-    "documentation",
-    "prettier",
-    "nodemon"
-  ];
+const [mkdir, writeFile, spawn] = [
+  fs.mkdir,
+  fs.writeFile,
+  child_process.spawn
+].map(promisify);
 
-  const { name, description } = await prompts([
-    {
-      type: "text",
-      name: "name",
-      message: "what do you call it?"
-    },
-    {
-      type: "text",
-      name: "description",
-      message: "what does it do?"
-    }
-  ]);
-  const pgk = resolve(`./packages/${name}`);
+const DEV_DEPENDENCIES = [
+  "@babel/core",
+  "@babel/preset-env",
+  "documentation",
+  "prettier",
+  "nodemon"
+];
 
-  await mkdir(pgk);
-  await mkdir(resolve(pgk, "lib"));
-  await Promise.all(
-    Object.keys(files).map(file =>
-      writeFile(
-        resolve(`${pgk}/${file}`),
-        files[file]({ name, description }),
-        "utf8"
+console.log("🥚 Oh! A package.");
+prompts([
+  {
+    type: "text",
+    name: "name",
+    message: "🥚 what do you call it?"
+  },
+  {
+    type: "text",
+    name: "description",
+    message: "🥚 what does it do?"
+  }
+])
+  .then(async ({ name, description }) => {
+    assert(name && name !== "", "name not provided");
+    assert(description && description !== "", "description not provided");
+
+    const pgk = resolve(`./packages/${name}`);
+
+    await mkdir(pgk);
+    await mkdir(resolve(pgk, "lib"));
+    await Promise.all(
+      Object.keys(files).map(file =>
+        writeFile(
+          resolve(`${pgk}/${file}`),
+          files[file]({ name, description }),
+          "utf8"
+        )
       )
-    )
-  );
-
-  const child = {
-    cwd: pgk,
-    stdio: ["inherit", "inherit", "inherit"]
-  };
-  await spawn("npm", ["install", "--save-dev", ...DEV_DEPENDENCIES], child);
-})(...[fs.mkdir, fs.writeFile, child_process.spawn].map(promisify));
+    );
+    await spawn("npm", ["install", "--save-dev", ...DEV_DEPENDENCIES], {
+      cwd: pgk,
+      stdio: ["inherit", "inherit", "inherit"]
+    });
+  })
+  .then(({ name, pkg }) =>
+    console.log(`🐣 @talon/${name} (${pkg}) was created`)
+  )
+  .catch(e => {
+    if (e.code === "EEXIST") return console.log("that package already exists!");
+    console.log(e);
+  });
