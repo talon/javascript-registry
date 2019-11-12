99729dbd6d3c145c65bf08fa6147499cffe7e12f
Talon <hire@talon.computer>
2019-11-12 17:31:04 -0500

    feat(Route.decode): also .withQuery
    affects: @talon/route

--- packages/route/lib/route.js
+++ packages/route/lib/route.js
@@ -1,5 +1,18 @@
-import { omit } from "ramda";
+import { omit, zip } from "ramda";
 import Url from "url";
+import QueryString from "querystring";
+
+const parts = route =>
+  route
+    .replace(/^\//, "")
+    .split("/")
+    .map(segment => {
+      if (segment.match(/:\w+/g)) {
+        return segment.substr(1);
+      } else {
+        return segment;
+      }
+    });
 
 /**
  * encode data into a route
@@ -16,12 +29,34 @@ import Url from "url";
  * @returns {array} `[path, data]` lefover data is returned as the second element in the array
  */
 export const encode = (route, data) => {
-  const params = route.match(/:\w+/g).map(x => x.substr(1));
+  const segments = parts(route);
 
   return [
-    params.reduce((url, param) => url.replace(`:${param}`, data[param]), route),
-    omit(params, data)
+    segments.reduce(
+      (url, param) => url.replace(`:${param}`, data[param]),
+      route
+    ),
+    omit(segments, data)
   ];
+};
+
+/**
+ * encode data into a route, include the querystring
+ *
+ * @example
+ *     const path = Route.withQuery("/api/v1/:id/items/:name", {
+ *        id: 1,
+ *        name: "keyboard",
+ *        limit: 20
+ *     })
+ *
+ * @param {string} route a route `/with/:params`
+ * @param {object} data keys will replace the `/path/:param`
+ * @returns {array} path including the querystring
+ */
+export const withQuery = (route, data) => {
+  const [path, query] = encode(route, data);
+  return `${path}?${QueryString.encode(query)}`;
 };
 
 /**
@@ -36,5 +71,8 @@ export const encode = (route, data) => {
  */
 export const decode = (route, path) => {
   const url = Url.parse(path, true);
-  const params = route.match(/:\w+/g).reduce((data, param, i) => {});
+  return Object.assign(
+    Object.fromEntries(zip(parts(route), parts(url.pathname))),
+    url.query
+  );
 };
--- packages/route/lib/route.test.js
+++ packages/route/lib/route.test.js
@@ -1,25 +1,31 @@
 import * as Route from "./route";
 
 test("Route.encode", () => {
-  const [path, params] = Route.encode("/:id/people/:name", {
+  const [path, data] = Route.encode("/:id/people/:name", {
     id: 1,
     name: "belle",
     limit: 1
   });
 
   expect(path).toBe("/1/people/belle");
-  expect(params).toEqual({ limit: 1 });
+  expect(data).toEqual({ limit: 1 });
 });
 
-test.skip("Route.decode", () => {
-  const [params, search] = Route.decode(
-    "/:id/people/:name",
-    "/1/people/belle?limit=1"
-  );
+test("Route.withQuery", () => {
+  expect(
+    Route.withQuery("/:id/people/:name", {
+      id: 1,
+      name: "belle",
+      limit: 1
+    })
+  ).toBe("/1/people/belle?limit=1");
+});
 
-  expect(params).toEqual({
-    id: 1,
-    name: "belle"
+test("Route.decode", () => {
+  expect(Route.decode("/:id/people/:name", "/1/people/belle?limit=1")).toEqual({
+    id: "1",
+    people: "people",
+    name: "belle",
+    limit: "1"
   });
-  expect(search).toEqual({ limit: 1 });
 });
