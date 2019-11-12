4b1a033d3408d94511b4007ed08d8f874f95e0e7
Talon <hire@talon.computer>
2019-11-12 00:16:21 -0500

    chore(docs): documentation generation
    affects: @talon/route

--- packages/route/lib/route.js
+++ packages/route/lib/route.js
@@ -1,6 +1,12 @@
 import { omit } from "ramda";
 import Url from "url";
 
+/**
+ * encode an Object in to a route
+ * @param route {string} a route /with/:params
+ * @param data {object} the object[key] will replace the /path/:param
+ * @returns {array} [path, data] lefover data is returned as the second element in the array
+ */
 export const encode = (route, data) => {
   const params = route.match(/:\w+/g).map(x => x.substr(1));
 
@@ -10,6 +16,12 @@ export const encode = (route, data) => {
   ];
 };
 
+/**
+ * decode an Object from a url
+ * @param route {string} a route /with/:params
+ * @param path {string} from which to extract data
+ * @returns {object} the data extracted from `path`
+ */
 export const decode = (route, path) => {
   const url = Url.parse(path, true);
   const params = route.match(/:\w+/g).reduce((data, param, i) => {});
