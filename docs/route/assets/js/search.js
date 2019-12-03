5d8ff9724a7224cac64f7a37580ff1a68c0dd96b
Talon <hire@talon.computer>
2019-12-02 23:03:29 -0500

    docs(docs): github pages

---
+++ docs/route/assets/js/search.js
@@ -0,0 +1,62 @@
+var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+  kinds: { "1": "External module", "64": "Function" },
+  rows: [
+    {
+      id: 0,
+      kind: 1,
+      name: '"route"',
+      url: "modules/_route_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 1,
+      kind: 64,
+      name: "encode",
+      url: "modules/_route_.html#encode",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    },
+    {
+      id: 2,
+      kind: 64,
+      name: "decode",
+      url: "modules/_route_.html#decode",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    },
+    {
+      id: 3,
+      kind: 64,
+      name: "withConstant",
+      url: "modules/_route_.html#withconstant",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    },
+    {
+      id: 4,
+      kind: 64,
+      name: "withBody",
+      url: "modules/_route_.html#withbody",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    },
+    {
+      id: 5,
+      kind: 64,
+      name: "keys",
+      url: "modules/_route_.html#keys",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    },
+    {
+      id: 6,
+      kind: 64,
+      name: "values",
+      url: "modules/_route_.html#values",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"route"'
+    }
+  ]
+};
