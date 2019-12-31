fe948de4af0ce1a35ef2a9c87238c990fef8b075
Talon Poole <hire@talon.computer>
2019-12-30 22:28:42 -0500

    fix(semantic release) possibly

--- release.config.js
+++ release.config.js
@@ -1,7 +1,10 @@
 module.export = () => ({
+    "release": {
+        "extends": ["semantic-release-monorepo"]
+    },
     "plugins": [
         ["@semantic-release/npm", {
-            pkgRoot: `${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}`
+            "pkgRoot": `${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}`
         }]
     ]
 })
