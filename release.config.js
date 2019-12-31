d8f40b7a7a14a5cd09afbd9904454c5a3df4c332
Talon Poole <hire@talon.computer>
2019-12-30 22:45:31 -0500

    fix(package json) commit version bumps to git I think...

--- release.config.js
+++ release.config.js
@@ -5,6 +5,9 @@ module.export = () => ({
     "plugins": [
         ["@semantic-release/npm", {
             "pkgRoot": `${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}`
+        }],
+        ["@semantic-release/git", {
+            "assets": `${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}/**`
         }]
     ]
 })
