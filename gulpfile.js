691fc9ef02dbaa4bb0a4acf3da76069320b8e87b
Talon Poole <hire@talon.computer>
2020-01-03 01:48:59 -0500

    feat(improve documentation): you get it
    affects: @talon/sip

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,8 @@ require("@babel/register")({
     rootMode: "upward"
 })
 
-const Mono = require("./packages/sip/lib/mono.js")
 const Sip = require("./packages/sip/lib")
+// @ts-ignore
 const meta = require("./package.json")
 
-exports.init = Mono.init({
-    repository: meta.repository.url,
-    directory: meta.workspaces[0],
-    registry: "https://npm.pkg.github.com",
-    scope: "talon"
-})
-
-Object.assign(exports, Sip.suite(`${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}`))
+Object.assign(exports, Sip.setup(`${__dirname}/packages/${process.env.LERNA_PACKAGE_NAME.split("/").slice(-1)}`))
