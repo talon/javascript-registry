b7b2877228957d2744dcbb0764f0065ea225bf28
talon <hire@talon.computer>
2020-02-14 00:28:59 -0500

    test(commit): add tests
    affects: @talon/git-conventions

---
+++ packages/git-conventions/lib/commit.js
@@ -0,0 +1,63 @@
+import dedent from "dedent-js"
+
+/**
+ * ```js
+ * import commit from "./lib/commit"
+ *
+ * describe("git-conventions commit", () => {
+ *   it("formats commits conventionally", () => {
+ *     expect(commit({
+ *       type: "feat",
+ *       description: "improve stuff",
+ *     })).toBe("feat: improve stuff")
+ *   })
+ *
+ *   it("handles optional scopes", () => {
+ *     expect(commit({
+ *       type: "feat",
+ *       scope: "sip",
+ *       description: "improve stuff",
+ *     })).toBe("feat(sip): improve stuff")
+ *   })
+ *
+ *   it("handles optional body", () => {
+ *     expect(commit({
+ *       type: "feat",
+ *       description: "improve stuff",
+ *       body: "you would not believe it",
+ *     })).toBe("feat: improve stuff\n\nyou would not believe it")
+ *   })
+ *
+ *   it("handles optional footer", () => {
+ *     expect(commit({
+ *       type: "feat",
+ *       description: "improve stuff",
+ *       body: "you would not believe it",
+ *       footer: "BREAKING CHANGE: all of it",
+ *     })).toBe("feat: improve stuff\n\nyou would not believe it\n\nBREAKING CHANGE: all of it")
+ *   })
+ * })
+ * ```
+ *
+ * @param {object} options - the commit options
+ * @param {string} options.type - the commit type
+ * @param {string} [options.scope] - the scope of the commit
+ * @param {string} options.description - a terse desription of the commit
+ * @param {string} [options.body] - a detailed explanation of the commit
+ * @returns {string} a conventional commit
+ */
+export default function({ type, scope, description, body, footer }) {
+  let commit = scope
+    ? `${type}(${scope}): ${description}`
+    : `${type}: ${description}`
+
+  if (body) {
+    commit = `${commit}\n\n${body}`
+  }
+
+  if (footer) {
+    commit = `${commit}\n\n${footer}`
+  }
+
+  return commit
+}
--- packages/git-conventions/lib/index.js
+++ packages/git-conventions/lib/index.js
@@ -3,39 +3,18 @@
 import dedent from "dedent-js"
 import shell from "shelljs"
 import yargs from "yargs"
-import path from "path"
+import commit from "./commit"
 
-/**
- * ```js
- * describe.skip("git-conventions", () => {
- *   it("commits", () => expect(true).toBeTruthy())
- *   it("releases", () => expect(true).toBeTruthy())
- * })
- * ```
- */
-yargs
-  .command(
-    "commit",
-    "Wraps `git commit` by generating a commit message from a few standard prompts based off of https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#-git-commit-guidelines",
-    function commit() {
-      const changedFiles = shell
-        .exec("git diff --cached --name-only", { silent: true })
-        .stdout.split("\n")
-
-      console.log(changedFiles)
-
-      // return dedent`${type}(${scope}): ${subject}
-
-      // ${body}
-
-      // ${footer}
-      // `
-    }
-  )
-  .command(
-    "release",
-    "Generate a CHANGELOG for each source folder and semantically versions the repository by tagging it",
-    function release() {
-      console.log("release")
-    }
-  ).argv
+yargs.command(
+  "commit",
+  "Wraps `git commit` by generating a commit message from a few standard prompts based off of https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#-git-commit-guidelines",
+  function() {
+    console.log(
+      commit({
+        type: "feat",
+        scope: "sip",
+        description: "improve stuff"
+      })
+    )
+  }
+).argv
